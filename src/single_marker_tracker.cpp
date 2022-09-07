8bd4f39861cb2c9183a75f8100973d76c31c8dd0
Błażej Sowa <bsowa123@gmail.com>
2022-09-07 13:10:50 +0000

    Rebase from 'debian/noetic/aruco_opencv'

--- src/single_marker_tracker.cpp
+++ src/single_marker_tracker.cpp
@@ -1,3 +1,5 @@
+#include <mutex>
+
 #include <opencv2/aruco.hpp>
 #include <opencv2/calib3d.hpp>
 
@@ -10,9 +12,11 @@
 #include <image_transport/camera_common.h>
 #include <image_transport/image_transport.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
 
 #include <aruco_opencv/ArucoDetectorConfig.h>
-#include <aruco_opencv/MarkerDetection.h>
+#include <aruco_opencv_msgs/MarkerDetection.h>
 
 namespace aruco_opencv {
 
@@ -40,6 +44,9 @@ class SingleMarkerTracker : public nodelet::Nodelet {
 
   // Parameters
   std::string cam_base_topic_;
+  std::string output_frame_;
+  bool transform_poses_;
+  bool publish_tf_;
   double marker_size_;
   int image_queue_size_;
 
@@ -60,6 +67,14 @@ class SingleMarkerTracker : public nodelet::Nodelet {
   cv::Ptr<cv::aruco::DetectorParameters> detector_parameters_;
   cv::Ptr<cv::aruco::Dictionary> dictionary_;
 
+  // Thread safety
+  std::mutex cam_info_mutex_;
+
+  // Tf2
+  tf2_ros::Buffer tf_buffer_;
+  tf2_ros::TransformListener *tf_listener_;
+  tf2_ros::TransformBroadcaster *tf_broadcaster_;
+
 public:
   SingleMarkerTracker()
       : camera_matrix_(3, 3, CV_64FC1), distortion_coeffs_(4, 1, CV_64FC1),
@@ -71,8 +86,16 @@ private:
     auto &pnh = getPrivateNodeHandle();
 
     retrieve_parameters(pnh);
+    transform_poses_ = !output_frame_.empty();
+
+    if (transform_poses_)
+      tf_listener_ = new tf2_ros::TransformListener(tf_buffer_);
+
+    if (publish_tf_)
+      tf_broadcaster_ = new tf2_ros::TransformBroadcaster();
 
     detector_parameters_ = cv::aruco::DetectorParameters::create();
+    // TODO: Add parameter for dictionary
     dictionary_ = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_ARUCO_ORIGINAL);
 
     dyn_srv_ = new dynamic_reconfigure::Server<aruco_opencv::ArucoDetectorConfig>(pnh);
@@ -88,10 +111,10 @@ private:
     it_ = new image_transport::ImageTransport(nh);
     pit_ = new image_transport::ImageTransport(pnh);
 
-    detection_pub_ = nh.advertise<aruco_opencv::MarkerDetection>("marker_detections", 5);
+    detection_pub_ = nh.advertise<aruco_opencv_msgs::MarkerDetection>("marker_detections", 5);
     debug_pub_ = pit_->advertise("debug", 1);
 
-    NODELET_INFO("Waiting for camera info...");
+    NODELET_INFO("Waiting for first camera info...");
 
     std::string cam_info_topic = image_transport::getCameraInfoTopic(cam_base_topic_);
     cam_info_sub_ =
@@ -103,8 +126,10 @@ private:
 
   void retrieve_parameters(ros::NodeHandle &pnh) {
     pnh.param<std::string>("cam_base_topic", cam_base_topic_, "camera/image_raw");
+    pnh.param<std::string>("output_frame", output_frame_, "");
+    pnh.param<bool>("publish_tf", publish_tf_, true);
     pnh.param<double>("marker_size", marker_size_, 0.15);
-    pnh.param<int>("image_queue_size", image_queue_size_, 5);
+    pnh.param<int>("image_queue_size", image_queue_size_, 1);
   }
 
   void reconfigure_callback(aruco_opencv::ArucoDetectorConfig &config, uint32_t level) {
@@ -131,10 +156,18 @@ private:
     detector_parameters_->maxErroneousBitsInBorderRate = config.maxErroneousBitsInBorderRate;
     detector_parameters_->minOtsuStdDev = config.minOtsuStdDev;
     detector_parameters_->errorCorrectionRate = config.errorCorrectionRate;
+#if CV_VERSION_MAJOR >= 4
+    detector_parameters_->cornerRefinementMethod = config.cornerRefinementMethod;
+#else
+    detector_parameters_->doCornerRefinement = config.cornerRefinementMethod == 1;
+#endif
+    detector_parameters_->cornerRefinementWinSize = config.cornerRefinementWinSize;
+    detector_parameters_->cornerRefinementMaxIterations = config.cornerRefinementMaxIterations;
+    detector_parameters_->cornerRefinementMinAccuracy = config.cornerRefinementMinAccuracy;
   }
 
   void callback_camera_info(const sensor_msgs::CameraInfo &cam_info) {
-    NODELET_INFO("Camera info retrieved.");
+    std::lock_guard<std::mutex> guard(cam_info_mutex_);
 
     cv::Mat cameraMatrixFromK(3, 3, CV_64FC1, 0.0);
     for (int i = 0; i < 9; ++i)
@@ -144,8 +177,10 @@ private:
     for (int i = 0; i < 4; ++i)
       distortion_coeffs_.at<double>(i, 0) = cam_info.D[i];
 
-    cam_info_retrieved_ = true;
-    cam_info_sub_.shutdown();
+    if (!cam_info_retrieved_) {
+      NODELET_INFO("First camera info retrieved.");
+      cam_info_retrieved_ = true;
+    }
   }
 
   void callback_image(const sensor_msgs::ImageConstPtr &img_msg) {
@@ -166,26 +201,57 @@ private:
     int n_markers = marker_ids.size();
     std::vector<cv::Vec3d> rvec_final(n_markers), tvec_final(n_markers);
 
-    aruco_opencv::MarkerDetection detection;
+    aruco_opencv_msgs::MarkerDetection detection;
     detection.header.frame_id = img_msg->header.frame_id;
     detection.header.stamp = img_msg->header.stamp;
 
+    cam_info_mutex_.lock();
     for (size_t i = 0; i < n_markers; i++) {
       int id = marker_ids[i];
 
-      std::vector<cv::Vec3d> rvecs(n_markers), tvecs(n_markers);
+#if CV_VERSION_MAJOR >= 4
+      cv::solvePnP(marker_obj_points_, marker_corners[i], camera_matrix_, distortion_coeffs_,
+                   rvec_final[i], tvec_final[i], false, cv::SOLVEPNP_IPPE_SQUARE);
+#else
+      cv::solvePnP(marker_obj_points_, marker_corners[i], camera_matrix_, distortion_coeffs_,
+                   rvec_final[i], tvec_final[i], false, cv::SOLVEPNP_ITERATIVE);
+#endif
 
-      cv::solvePnPGeneric(marker_obj_points_, marker_corners[i], camera_matrix_, distortion_coeffs_,
-                          rvecs, tvecs, false, cv::SOLVEPNP_IPPE_SQUARE);
-
-      // Choose the solution with lower reprojection error
-      rvec_final[i] = rvecs[0];
-      tvec_final[i] = tvecs[0];
-
-      MarkerPose mpose;
+      aruco_opencv_msgs::MarkerPose mpose;
       mpose.marker_id = id;
       mpose.pose = convert_rvec_tvec(rvec_final[i], tvec_final[i]);
       detection.markers.push_back(mpose);
+    }
+    cam_info_mutex_.unlock();
+
+    if (transform_poses_ && n_markers > 0) {
+      detection.header.frame_id = output_frame_;
+      geometry_msgs::TransformStamped cam_to_output;
+      // Retrieve camera -> output_frame transform
+      try {
+        cam_to_output = tf_buffer_.lookupTransform(output_frame_, img_msg->header.frame_id,
+                                                   img_msg->header.stamp, ros::Duration(1.0));
+      } catch (tf2::TransformException &ex) {
+        ROS_WARN("%s", ex.what());
+        return;
+      }
+      for (auto &marker_pose : detection.markers)
+        tf2::doTransform(marker_pose.pose, marker_pose.pose, cam_to_output);
+    }
+
+    if (publish_tf_ && n_markers > 0) {
+      std::vector<geometry_msgs::TransformStamped> transforms;
+      for (auto &marker_pose : detection.markers) {
+        geometry_msgs::TransformStamped transform;
+        transform.header.stamp = detection.header.stamp;
+        transform.header.frame_id = detection.header.frame_id;
+        transform.child_frame_id = std::string("marker_") + std::to_string(marker_pose.marker_id);
+        tf2::Transform tf_transform;
+        tf2::fromMsg(marker_pose.pose, tf_transform);
+        transform.transform = tf2::toMsg(tf_transform);
+        transforms.push_back(transform);
+      }
+      tf_broadcaster_->sendTransform(transforms);
     }
 
     detection_pub_.publish(detection);
@@ -194,9 +260,15 @@ private:
       auto debug_cv_ptr = cv_bridge::toCvCopy(img_msg, "bgr8");
       cv::aruco::drawDetectedMarkers(debug_cv_ptr->image, marker_corners, marker_ids);
       for (size_t i = 0; i < marker_ids.size(); i++) {
+#if CV_VERSION_MAJOR >= 4
         cv::drawFrameAxes(debug_cv_ptr->image, camera_matrix_, distortion_coeffs_, rvec_final[i],
                           tvec_final[i], 0.2, 3);
+#else
+        cv::aruco::drawAxis(debug_cv_ptr->image, camera_matrix_, distortion_coeffs_, rvec_final[i],
+                            tvec_final[i], 0.2);
+#endif
       }
+
       debug_pub_.publish(debug_cv_ptr->toImageMsg());
     }
 
